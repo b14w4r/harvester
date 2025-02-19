21e8923a337211582721256f7d6c4c82b458a92f
b14w4r <spiritedbeaver@yandex.ru>
2025-02-19 16:34:48 +0300

    env fix, table formating init

--- test.py
+++ test.py
@@ -1,8 +1,11 @@
 import imaplib
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
-IMAP_SERVER = "imap.gmail.com"
-EMAIL_ACCOUNT = "zepovj@gmail.com"
-EMAIL_PASSWORD = "Gojda2025"
+IMAP_SERVER = os.environ['IMAP_SERVER']
+EMAIL_ACCOUNT = os.environ['EMAIL_ACCOUNT']
+EMAIL_PASSWORD = os.environ['EMAIL_PASSWORD']
 
 mail = imaplib.IMAP4_SSL(IMAP_SERVER)
 try:
