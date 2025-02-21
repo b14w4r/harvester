a0055710a6a2218eb738baa8a20fb74b4e2df534
b14w4r <spiritedbeaver@yandex.ru>
2025-02-21 08:52:09 +0300

    file reator added

--- injector.py
+++ injector.py
@@ -1,7 +1,8 @@
 import sqlalchemy as db
 
 def injection(table):
-    connection = db.create_engine("postgresql://myuser:mypassword@localhost:5432/mydb")
+    # connection = db.create_engine("postgresql://myuser:mypassword@localhost:5432/mydb")
+    connection = db.create_engine("postgresql://neondb_owner:npg_FBvTi18ySpoY@ep-yellow-salad-a9lbdmij-pooler.gwc.azure.neon.tech/neondb?sslmode=require")
     table.to_sql(
         "prices",
         connection,
--- test.py
+++ test.py
@@ -1,15 +1,30 @@
-import imaplib
-import os
-from dotenv import load_dotenv
-load_dotenv()
+import random
+import pandas as pd
+from datetime import datetime
 
-IMAP_SERVER = os.environ['IMAP_SERVER']
-EMAIL_ACCOUNT = os.environ['EMAIL_ACCOUNT']
-EMAIL_PASSWORD = os.environ['EMAIL_PASSWORD']
+# Генерация одинаковых случайных id для всех файлов
+fixed_ids = random.sample(range(50, 150), 5)
 
-mail = imaplib.IMAP4_SSL(IMAP_SERVER)
-try:
-    status, response = mail.login(EMAIL_ACCOUNT, EMAIL_PASSWORD)
-    print(f"Login status: {status}, response: {response}")
-except imaplib.IMAP4.error as e:
-    print(f"Ошибка IMAP: {e}")
+for i in range(10):
+    # Генерация случайной даты с разными месяцами в 2025 году
+    month = random.randint(1, 12)
+    date = datetime(2025, month, 20).strftime("%d.%m.%Y")
+
+    # Генерация случайных данных для id и цены
+    data = {
+        "id": fixed_ids,  # 5 одинаковых случайных id во всех файлах
+        "Цена за единицу": [random.randint(50, 1000) for _ in range(5)]
+    }
+    df = pd.DataFrame(data)
+
+    # Создание нового файла xlsx
+    file_name = f"file_{i + 1}.xlsx"
+    with pd.ExcelWriter(file_name, engine="xlsxwriter") as writer:
+        # Запись заголовка и даты в первую строку
+        df_meta = pd.DataFrame([["Дата актуальности", date]])
+        df_meta.to_excel(writer, sheet_name="Sheet1", index=False, header=False, startrow=0)
+
+        # Запись основной таблицы начиная с 2-й строки
+        df.to_excel(writer, sheet_name="Sheet1", index=False, startrow=1)
+
+    print(f"Файл {file_name} создан.")
